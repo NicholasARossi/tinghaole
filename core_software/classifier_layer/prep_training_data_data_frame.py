293f413f76d6404f6426f261ea13a87bd09fd739
Nicholas.A.Rossi <nicholas.rossi2@gmail.com>
2022-08-01 21:30:59 -0700

    update

---
+++ core_software/classifier_layer/prep_training_data_data_frame.py
@@ -0,0 +1,180 @@
+import librosa
+import numpy as np
+import os
+from glob import glob
+import warnings
+warnings.filterwarnings("ignore", message="PySoundFile failed. Trying audioread instead.")
+from tqdm import tqdm
+import random
+import tensorflow_io as tfio
+import pandas as pd
+
+
+class AugmentedDataGenerator:
+
+    def __init__(self,data_dir,out_target='../../data/full_augmented_data_seperate',augment_multiplier=10):
+        self.data_dir=data_dir
+        self.augment_multiplyer=augment_multiplier
+        self.out_target=out_target
+        if not os.path.exists(out_target):
+            os.mkdir(out_target)
+        self.data_max_dims = (80, 60)
+
+
+    def _load_raw_files(self):
+
+        files = glob(f'{self.data_dir}*.mp3')
+        ## suffle order of files
+        random.shuffle(files)
+        self.files=files
+
+
+    def _iterate_file_list(self):
+
+        ### loading original dataset
+        MSGs = []
+        full_labels = []
+        data_frame=pd.DataFrame(columns=['id','tone','phoneme'])
+
+
+        sample_counter=0
+        for file in tqdm(self.files):
+
+            if self.augment_multiplyer==1:
+                masked = mp3toMSG(file, trimming=True)
+
+                # masked = tfio.audio.time_mask(MSG, param=10).numpy()
+                # masked = tfio.audio.freq_mask(masked, param=10).numpy()
+                # full_labels.extend([file.split('/')[-1].replace('_MP3.mp3', '')]*len(augmented))
+                padded_data = add_padding(np.expand_dims(masked, -1), bonus_padding=2, maxes=self.data_max_dims)[0]
+                title = f'sample_{sample_counter}.npy'
+
+                fragment = file.split('/')[-1].split('_')[0]
+                np.save(os.path.join(self.out_target, title), padded_data)
+                data_frame = data_frame.append({'id': title, 'tone': fragment[-1], 'phoneme': fragment[:-1]},
+                                               ignore_index=True)
+                sample_counter += 1
+
+            else:
+                for n in range(self.augment_multiplyer):
+                    failed = True
+
+                    while failed==True:
+                        ### hacky fix, data augmentation has a random chance to fail
+                        try:
+                            MSG=mp3toMSG(file,trimming=True)
+
+                            masked = tfio.audio.time_mask(MSG, param=10).numpy()
+                            masked = tfio.audio.freq_mask(masked, param=10).numpy()
+
+                            failed=False
+                        except:
+                            print(f'{file} failed')
+
+                    # full_labels.extend([file.split('/')[-1].replace('_MP3.mp3', '')]*len(augmented))
+                    padded_data = add_padding(np.expand_dims(masked,-1), bonus_padding=2, maxes=self.data_max_dims)[0]
+                    title=f'sample_{sample_counter}.npy'
+
+                    fragment=file.split('/')[-1].split('_')[0]
+                    np.save(os.path.join(self.out_target,title), padded_data)
+                    data_frame=data_frame.append({'id':title,'tone':fragment[-1],'phoneme':fragment[:-1]},ignore_index=True)
+                    sample_counter+=1
+
+        data_frame.to_csv(os.path.join(self.out_target,'mapping.csv'))
+
+
+    def _serialize_data(self):
+
+
+        MSG_dir=os.path.join(self.out_target,'MSG_chunks')
+        Label_dir=os.path.join(self.out_target,'Label_chunks')
+        for folder in [MSG_dir,Label_dir]:
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+
+        MSG_chunks = list(self.chunker(self.MSGs, self.chunk_size))
+        Label_chunks= list(self.chunker(self.full_labels, self.chunk_size))
+
+        chunk_number=0
+        for MSG_chunk,Label_chunk in tqdm(zip(MSG_chunks,Label_chunks)):
+
+            padded_data = add_padding(MSG_chunk, bonus_padding=2, maxes=self.data_max_dims)
+
+
+            np.save(os.path.join(MSG_dir,f'chunk_{chunk_number:02d}.npy'), padded_data)
+            np.save(os.path.join(Label_dir,f'chunk_{chunk_number:02d}.npy'), Label_chunk)
+
+            chunk_number+=1
+
+    def run_agumentation(self):
+        self._load_raw_files()
+        self._iterate_file_list()
+
+
+
+    @staticmethod
+    def chunker(lst, n):
+        """Yield successive n-sized chunks from lst."""
+        for i in range(0, len(lst), n):
+            yield lst[i:i + n]
+
+
+def trim_leading_trailing_silence(audio):
+    trimmed_sound = librosa.effects.trim(audio, top_db=20, frame_length=256, hop_length=64)[0]
+    return trimmed_sound
+
+# prepping training data
+def mp3tomfcc(file_path):
+    audio, sample_rate = librosa.core.load(file_path)
+
+
+
+
+    mfcc = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=60)
+    return mfcc
+
+
+def mp3toMSG(file_path,trimming=True):
+    audio, sample_rate = librosa.core.load(file_path)
+
+    if trimming==True:
+        audio=trim_leading_trailing_silence(audio)
+
+    MSG = librosa.feature.melspectrogram(y=audio, sr=sample_rate, n_fft=1024, hop_length=512, n_mels=80, fmin=75,
+                                         fmax=3700)
+    MSG = np.log10(MSG + 1e-10)
+
+    return MSG
+
+
+def add_padding(arrays, bonus_padding=10,maxes=None):
+    if maxes is None:
+        heights, widths = list(zip(*[x.shape for x in arrays]))
+        max_height = max(heights)
+        max_width = max(widths)
+
+    else:
+        max_height,max_width=maxes[0],maxes[1]
+
+    new_arrays = []
+    for array in arrays:
+        pad_width = (max_width - array.shape[1]) + bonus_padding
+        pad_height = (max_height - array.shape[0]) + bonus_padding
+
+        new_array = np.pad(array, pad_width=((bonus_padding, pad_height), (bonus_padding, pad_width)), mode='constant')
+        new_arrays.append(new_array)
+
+    heights, widths = list(zip(*[x.shape for x in new_arrays]))
+
+    assert len(set(heights)) == 1, print('not all the same size!')
+    assert len(set(widths)) == 1, print('not all the same size!')
+    return new_arrays
+
+
+
+
+
+
+if __name__ == '__main__':
+    augmentation_engine=AugmentedDataGenerator('../../data/tone_perfect_mp3/')
+    augmentation_engine.run_agumentation()
